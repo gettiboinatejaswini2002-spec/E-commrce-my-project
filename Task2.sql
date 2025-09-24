599afaca202ce3939008c7792fe2b0beea6ca9b0
gettiboinatejaswini2002-spec <gettiboinatejaswini2002@gmail.com>
2025-09-24 22:05:03 +0530

    Task2.sql

---
+++ Task2.sql
@@ -0,0 +1,27 @@
+  create table patient
+              (
+               pid int,
+               fname varchar(40),
+               lname varchar(40),
+               age int,
+               bg varchar(40)
+               )
+               Select * from patient
+               insert into patient values (1,'madhava','reddy',45,'o+ve')
+               insert into patient values (2,'abhinav','bandra',45,'o-ve')
+               insert into patient values (4,'hari','kiran',60,'b-ve')
+               insert into patient values (3,'madhava','kiran',52,'o+ve')
+               insert into patient values (5,'veena','kumari',42,NULL)
+               insert into patient values (6,'k_iran','kumari',39,'b-ve')
+               insert into patient values (2,'abhinav','bandra',45,'o-ve')
+               insert into patient values (7,'mahes%h','nambootri',36,'b+ve')
+               insert into patient values (8,'rahul','kumar',46,'b-ve')
+               insert into patient values (9,'bharat','kumar',56,'b-ve')
+     insert into patient (pid,fname,lname,age,bg) values (10,'ahana','kumar',78,'o+ve')
+    insert into patient (pid,fname,lname,age,bg) values (11,'silli','suresh',81,null)
+     SET SQL_SAFE_UPDATES=0
+    update patient set fname='raja',lname='raveender',age=66,bg='o-ve' where pid=10
+    update patient set fname='menna',lname='kumari' where pid =11
+    delete from patient where pid=10
+    delete from patient where pid =11
+    SET SQL_SAFE_UPDATES=1 
